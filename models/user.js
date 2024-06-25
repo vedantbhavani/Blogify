1d314f2a0f92d7925cc27adbe6132cda6226a492
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-06-25 11:15:39 +0530

    change

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const { createHmac , randomBytes } = require('crypto');
 const {Schema , model}= require('mongoose')
-
 const mongoose = require('mongoose');
 const { createTokensForUser } = require('../services/authentication');
 
@@ -39,35 +38,26 @@ const userSchema = new Schema({
         default : "USER" 
     }
 })
-
 userSchema.pre("save" , function(next){
     const user = this;
     if(!user.isModified('password')) return;
-
     const salt = randomBytes(16).toString();
     const hashPassword = createHmac('sha256' , salt)
     .update(user.password)
     .digest('hex')    
-
     this.salt = salt;
     this.password = hashPassword;
-
     next()
 })
-
 userSchema.static('matchPasswordAndGenerateTokens' , async function(email , password){
     const user = await this.findOne({ email })
     if(!user) throw new Error('User Not Found!')
-
     const salt = user.salt;
     const hashedPassword = user.password;
-
     const userProvideHash = createHmac('sha256' , salt)
     .update(password)
     .digest('hex')    
-
     if(hashedPassword !== userProvideHash) throw new Error('Incorrect Password!')
-
     const token = createTokensForUser(user)
     return token;
 })
